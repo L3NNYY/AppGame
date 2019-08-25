e9872c491055ab4d48b4d17c92079a3c6eb3e720
yoMan <your@man.wagwan>
2019-08-25 03:41:30 +0100

    Fixing tingz

--- DeGame/Assets/asteroid_float.cs
+++ DeGame/Assets/asteroid_float.cs
@@ -5,53 +5,77 @@ using System;
 
 public class asteroid_float : MonoBehaviour
 {
-    Vector3 mousePos;
+    Vector2 mousePos;
+    Vector2 centre;
     Animator anim;
     CircleCollider2D collider;
+    bool isMoving = true;
     // Start is called before the first frame update
     void Start()
     {
         collider = gameObject.GetComponent<CircleCollider2D>();
         anim = gameObject.GetComponent<Animator>();
+        centre = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        mousePos = Input.mousePosition;
-        Vector3 mousePoint = Camera.main.ScreenToWorldPoint(mousePos);
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // print(centre);
+        //print(mousePos);
+        if (isMoving)
+        {
+            movement();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            CollideChecker(mousePos);
+        }
+
+    }
+
+
+    void CollideChecker(Vector2 mousePoint)
+    {
+        if (collider.OverlapPoint(mousePoint))
+        {
+            anim.SetTrigger("Active");
+            isMoving = false;
+            Destroy(this.gameObject, 1.0f);
+        }
+
+    }
+    void movement()
+    {
         float x_velocity;
         float y_velocity;
-        if(mousePoint.x > transform.position.x){
+        if (centre.x > transform.position.x)
+        {
             x_velocity = 2f;
         }
-        else if(mousePoint.x < transform.position.x){
-             x_velocity = -2f;
+        else if (centre.x < transform.position.x)
+        {
+            x_velocity = -2f;
         }
-        else{
+        else
+        {
             x_velocity = 0f;
         }
 
-        if(mousePoint.y > transform.position.y){
+        if (centre.y > transform.position.y)
+        {
             y_velocity = 2f;
         }
-        else if(mousePoint.y < transform.position.y){
+        else if (centre.y < transform.position.y)
+        {
             y_velocity = -2f;
         }
-        else{
+        else
+        {
             y_velocity = 0f;
         }
-        transform.Translate(x_velocity*Time.deltaTime,y_velocity*Time.deltaTime,0f);
-        if(Input.GetMouseButtonDown(0)){
-            CollideChecker(mousePoint);
-        }
-        
-    }
-
-    void CollideChecker(Vector3 mousePoint){
-        if(collider.OverlapPoint(mousePoint)){
-            anim.SetTrigger("Active");
-        }
-
+        transform.Translate(x_velocity * Time.deltaTime, y_velocity * Time.deltaTime, 0f);
     }
 }
+ 
