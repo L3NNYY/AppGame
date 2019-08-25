8484fa7293a9ba0a6ad6960381ced57ba03bb742
JakubLenny <jlenski2000@gmail.com>
2019-08-25 02:08:16 +0100

    Added explosion animation on click

--- DeGame/Assets/asteroid_float.cs
+++ DeGame/Assets/asteroid_float.cs
@@ -5,38 +5,53 @@ using System;
 
 public class asteroid_float : MonoBehaviour
 {
-    Vector3 mousepos;
+    Vector3 mousePos;
+    Animator anim;
+    CircleCollider2D collider;
     // Start is called before the first frame update
     void Start()
     {
+        collider = gameObject.GetComponent<CircleCollider2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {   
-        mousepos = Input.mousePosition;
-        Vector3 mousepoint = Camera.main.ScreenToWorldPoint(mousepos);
+        mousePos = Input.mousePosition;
+        Vector3 mousePoint = Camera.main.ScreenToWorldPoint(mousePos);
         float x_velocity;
         float y_velocity;
-        if(mousepoint.x > transform.position.x){
+        if(mousePoint.x > transform.position.x){
             x_velocity = 2f;
         }
-        else if(mousepoint.x < transform.position.x){
+        else if(mousePoint.x < transform.position.x){
              x_velocity = -2f;
         }
         else{
             x_velocity = 0f;
         }
 
-        if(mousepoint.y > transform.position.y){
+        if(mousePoint.y > transform.position.y){
             y_velocity = 2f;
         }
-        else if(mousepoint.y < transform.position.y){
+        else if(mousePoint.y < transform.position.y){
             y_velocity = -2f;
         }
         else{
             y_velocity = 0f;
         }
         transform.Translate(x_velocity*Time.deltaTime,y_velocity*Time.deltaTime,0f);
+        if(Input.GetMouseButtonDown(0)){
+            CollideChecker(mousePoint);
+        }
+        
+    }
+
+    void CollideChecker(Vector3 mousePoint){
+        if(collider.OverlapPoint(mousePoint)){
+            anim.SetTrigger("Active");
+        }
+
     }
 }
