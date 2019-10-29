f7e34949a20ebacfcfc16922172601853eb2fce8
JakubLenny <jlenski2000@gmail.com>
2019-10-29 00:04:15 +0000

    Unlocks are now saved
    Once a planet is unlocked it is saved to a string array.

--- DeGame/Assets/Scripts/unlock_manager.cs
+++ DeGame/Assets/Scripts/unlock_manager.cs
@@ -12,23 +12,58 @@ public class unlock_manager : MonoBehaviour
     void Start()
     {
         coin = gameObject.AddComponent<coins>();
-        coin.changeCoins(9999);
         coin_value.text = "" + coin.getCoins();
+        showUnlockedSkins();
+        notifySelect(PlayerPrefs.GetString("planet_texture"));
     }
 
-    public void UnlockChosen(GameObject skin){
-        int value = System.Convert.ToInt32(skin.transform.GetChild(0).GetComponent<Text>().text);
-        if(coin.getCoins() < value){
-            print("Insufficient coin amount");
-            return;
+    void showUnlockedSkins()
+    {
+        print(PlayerPrefs.GetString("unlocked_planets"));
+        string[] unlockedPlanets = PlayerPrefs.GetString("unlocked_planets").Split(',');
+        foreach (var go in FindObjectsOfType(typeof(GameObject)) as GameObject[])
+        {
+            if (go.tag == "unlock")
+            {
+                Image texture = go.GetComponent<Image>();
+                foreach (string planet in unlockedPlanets)
+                {
+                    if (texture.mainTexture.name == planet)
+                    {
+                        print(planet + " unlocked");
+                        go.transform.GetChild(0).GetComponent<Text>().text = "UNLOCKED";
+                    }
+                }
+            }
         }
-        Image texture = skin.GetComponent<Image>();
-        PlayerPrefs.SetString("planet_texture",texture.mainTexture.name);
-        coin.changeCoins(-value);
-        coin_value.text = "" + coin.getCoins();
     }
-
-    public void BackToMenu(){
+    public void UnlockChosen(GameObject skin)
+    {
+        Image texture = skin.GetComponent<Image>();
+        if (skin.transform.GetChild(0).GetComponent<Text>().text != "UNLOCKED")
+        {
+            int value = System.Convert.ToInt32(skin.transform.GetChild(0).GetComponent<Text>().text);
+            if (coin.getCoins() < value)
+            {
+                print("Insufficient coin amount");
+                return;
+            }
+            string unlockedPlanets = PlayerPrefs.GetString("unlocked_planets");
+            unlockedPlanets = unlockedPlanets + "," + texture.mainTexture.name;
+            PlayerPrefs.SetString("unlocked_planets", unlockedPlanets);
+            coin.changeCoins(-value);
+            coin_value.text = "" + coin.getCoins();
+            skin.transform.GetChild(0).GetComponent<Text>().text = "UNLOCKED";
+        };
+        PlayerPrefs.SetString("planet_texture", texture.mainTexture.name);
+        notifySelect(PlayerPrefs.GetString("planet_texture"));
+    }
+    void notifySelect(string name){
+        GameObject notify_text = GameObject.Find("skin_notify");
+        notify_text.GetComponent<Text>().text = "Current planet: " + name;
+    }
+    public void BackToMenu()
+    {
         SceneManager.LoadScene("MenuScene");
     }
 }
